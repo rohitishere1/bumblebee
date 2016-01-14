55e70ab9dec39536ea8872ce9e18c879ecb6017b
Rohit Singh <rohit.singh@paytm.com>
2016-01-14 16:18:24 +0530

    initial commit with entry point and other

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/**
+ * @param skeleton {Object}
+ * @param data {Object}
+ * @sample skeleton = {
+ * "L0:L1:L2" : "L0:L1"
+ * }
+ */
+var transform = function trx (skeleton, data) {
+  
+};
